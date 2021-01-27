dd26f91c5f7ef6caad0a413a44546aa3b74c3be2
Daniel Ribeiro <drsantosribeiro@gmail.com>
2021-01-26 23:09:10 -0300

    Implementação dos Modelos do projeto e rotas.

---
+++ SmartSchool/SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        public List<Aluno> Alunos = new List<Aluno>() {
+            new Aluno()
+            {
+                Id = 1,
+                Nome = "Solange",
+                Sobrenome = "Santos",
+                Telefone = "32418586"
+            },
+            new Aluno()
+            {
+                Id = 2,
+                Nome = "Tania",
+                Sobrenome = "Regina",
+                Telefone = "32411608"
+            },
+            new Aluno()
+            {
+                Id = 3,
+                Nome = "Geraldo",
+                Sobrenome = "Parreiras",
+                Telefone = "32412234"
+            },
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        // GET api/<AlunoController>/5
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+
+            if (aluno == null)
+            {
+                return BadRequest("Aluno não foi encontrado");
+            }
+
+            return Ok(aluno);
+        }
+
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome));
+
+            if (aluno == null)
+            {
+                return BadRequest("Aluno não foi encontrado");
+            }
+
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        //// GET: api/<AlunoController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<AlunoController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<AlunoController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<AlunoController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE api/<AlunoController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
